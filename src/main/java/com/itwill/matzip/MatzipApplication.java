ffc3b7c6e7946ab0af8b40492b0646b7a98ada4e
wooruk <wooruk@MacBook-Pro-de-wooruk.local>
2024-01-28 23:05:47 -0800

    test:push 안되는 문제 해결

--- src/main/java/com/itwill/matzip/MatzipApplication.java
+++ src/main/java/com/itwill/matzip/MatzipApplication.java
@@ -2,7 +2,9 @@ package com.itwill.matzip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MatzipApplication {
 
