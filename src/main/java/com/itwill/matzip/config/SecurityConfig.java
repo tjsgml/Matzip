5571ccf758d619246bf2cfa412a8a52b8c043cc4
Kim Yeonsu <dustn7020@naver.com>
2024-01-31 15:49:37 +0900

    카카오 로그인 정보 가져오기

--- src/main/java/com/itwill/matzip/config/SecurityConfig.java
+++ src/main/java/com/itwill/matzip/config/SecurityConfig.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
 	
