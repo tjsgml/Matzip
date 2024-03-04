6a146a6f987cf371f3cc1b66a9a2d3e5ec1aac6f
tlacodnjs667 <tlacodnjs667@gmail.com>
2024-03-03 20:57:45 -0800

    feat/add-security-dep-in-admin

--- src/main/java/com/itwill/matzip/web/admin/AdminMatzipController.java
+++ src/main/java/com/itwill/matzip/web/admin/AdminMatzipController.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,7 @@ import java.util.Map;
 @Slf4j
 @RequiredArgsConstructor
 @Controller
+@PreAuthorize("hasRole('ADMIN')")
 @RequestMapping("/admin/matzip")
 public class AdminMatzipController {
 
