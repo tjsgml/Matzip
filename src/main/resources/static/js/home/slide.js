f40dc011f33c8d1431420cf295ddaf4ae45434cf
Kim Yeonsu <dustn7020@naver.com>
2024-03-04 16:53:56 +0900

    홈 기능 수정

--- src/main/resources/static/js/home/slide.js
+++ src/main/resources/static/js/home/slide.js
@@ -11,42 +11,44 @@ document.addEventListener('DOMContentLoaded', function() {
 	const carouselLength = [];
 
 	for (let box of boxes) {
-		const num = box.getAttribute('data-id');
-		cnt[num] = 0;
 
-		container[num] = box.querySelector('ul');
-		carouselLength[num] = box.querySelector('ul').children.length;
+		if (box.querySelector('ul').children.length > 3) {
+			const num = box.getAttribute('data-id');
+			cnt[num] = 0;
 
-		const prevBtn = box.querySelector('button.preBtn');
-		const nextBtn = box.querySelector('button.nextBtn');
+			container[num] = box.querySelector('ul');
+			carouselLength[num] = box.querySelector('ul').children.length;
 
-		prevBtn.addEventListener("click", movetoLeft);
-		nextBtn.addEventListener("click", movetoRight);
+			const prevBtn = box.querySelector('button.preBtn');
+			const nextBtn = box.querySelector('button.nextBtn');
+
+			prevBtn.addEventListener("click", movetoLeft);
+			nextBtn.addEventListener("click", movetoRight);
+		}
 	}
 
 	//--------------------------------------------------------------------------------------------
 	//왼쪽 버튼을 클릭 시 동작
 	function movetoLeft(e) {
 		const num = e.target.getAttribute('data-id');
-		if (cnt[num] != 0) {
-			cnt[num]++;
-			translateContainer(num);
+		if (carouselLength[num] > 3) {
+			translateContainer(num, 1);
 		}
 	}
 
 	//오른쪽 버튼을 클릭 시 동작
 	function movetoRight(e) {
 		const num = e.target.getAttribute('data-id');
-
-		if (cnt[num] != -carouselLength[num] + 3 && carouselLength[num] > 3) {
-			cnt[num]--;
-			translateContainer(num);
+		if (carouselLength[num] > 3) {
+			translateContainer(num, -1);
 		}
 	}
 
 	//ul의 위치를 이동시킴
-	function translateContainer(num) {
-		container[num].style.transform = `translateX(${(215 * cnt[num])}px)`;
+	function translateContainer(num, direction) {
+		direction == 1
+			? container[num].insertBefore(container[num].lastElementChild, container[num].firstElementChild)
+			: container[num].appendChild(container[num].firstElementChild);
 	}
 
 });
