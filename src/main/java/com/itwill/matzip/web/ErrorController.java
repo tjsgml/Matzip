fd4b3b42eb088a0b5bb8a662f47311138d221470
gyeommii <yydasol@gmail.com>
2024-03-10 20:49:31 +0900

    [에러컨트롤러] customError.html 수정중

--- src/main/java/com/itwill/matzip/web/ErrorController.java
+++ src/main/java/com/itwill/matzip/web/ErrorController.java
@@ -4,44 +4,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice // 모든 컨트롤러에서 발생하는 예외 처리 가능
-public class ErrorController{
+public class ErrorController {
 	
-	@ExceptionHandler(Exception.class) // @ExceptionHandler 특정 예외 유형 처리 메서드 정의  -> Exception.class를 인자로 받아, *모든 종류의 예외*를 처리
+	@ExceptionHandler(Exception.class)
 	public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
 		HttpStatus status = getStatus(req);
-		
+
 		ModelAndView mav = new ModelAndView();
-		mav.addObject("errorStatus", status.value()); // http상태코드
+		mav.addObject("errorStatus", status.value()); // http 상태코드 
 		mav.addObject("errorMessage", status.getReasonPhrase()); // 상태 메시지
 		mav.addObject("errorDetail", e.getMessage()); // 예외 메시지
-		
-		switch (status.value()) { // 에러 이미지
-	        case 404:
-	            mav.addObject("errorImage", "/images/errors/404.png");
-	            break;
-	        case 500:
-	            mav.addObject("errorImage", "/images/errors/500.png");
-	            break;
-	        default:
-	            mav.addObject("errorImage", "/images/errors/default.png");
-	    }
-	
-	    mav.setViewName("error/customError");
-	    return mav;
+
+		// ErrorController 내부에 추가
+		if (status.is4xxClientError()) {
+			mav.addObject("errorImage", "/img/errors.jpg");
+			mav.addObject("userMessage", "요청하신 페이지를 찾을 수 없습니다. URL을 확인해주세요. \r\n"
+					+ "						문제가 계속되면 홈페이지로 돌아가 다른 정보를 찾아보시는 것을 권장합니다"); // 400번대 에러 메시지
+		} else if (status.is5xxServerError()) {
+			mav.addObject("errorImage", "/img/errors.jpg"); // "/images/errors/5xx.png"
+			mav.addObject("userMessage", "오우! 문제가 있군요! 관리자에게 문의 해주세요!"); // 500번대 에러 메시지
+		} else {
+			mav.addObject("errorImage", "/img/errors.jpg");
+			mav.addObject("userMessage", "알 수 없는 에러가 발생했습니다."); // 기본 에러 메시지
+		}
+
+		mav.setViewName("error/customError");
+		return mav;
 	}
 
+
 	private HttpStatus getStatus(HttpServletRequest request) {
-	    Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code"); // 상태코드 가져옴
-	    if (statusCode == null) { 
-	        return HttpStatus.INTERNAL_SERVER_ERROR; // 상태코드 null이면 500으로
-	    }
-	    return HttpStatus.valueOf(statusCode); // 해당 정수 값에 해당하는 HttpStatus 열거형 상수를 반환
+		Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+		if (statusCode == null) {
+			return HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		HttpStatus status = HttpStatus.valueOf(statusCode);
+
+		// 400번대 에러 처리
+		if (status.is4xxClientError()) {
+			return HttpStatus.BAD_REQUEST; // 400번대 에러는 모두 BAD_REQUEST로 처리
+		}
+		// 500번대 에러 처리
+		else if (status.is5xxServerError()) {
+			return HttpStatus.INTERNAL_SERVER_ERROR; // 500번대 에러는 모두 INTERNAL_SERVER_ERROR로 처리
+		}
+		return status;
 	}
-	
-	
+
 }
